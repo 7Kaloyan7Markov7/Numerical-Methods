b6b66583b820bdbecddfa549e5d1696a5b1749dd
Kalata <153134271+7Kaloyan7Markov7@users.noreply.github.com>
2024-12-13 13:46:50 +0200

    Update task1.py

--- Week 1/task1.py
+++ Week 1/task1.py
@@ -9,9 +9,6 @@ def g(x):
     return 1 + x + 0.5*x**2 + 0.1667*x**3
 
 x_range = np.linspace(-0.5, 0.5, 1000)
-y_res_f = f(x_range)
-y_res_g = g(x_range)
-
 plt.plot(x_range, f(x_range), x_range, g(x_range))
 
 plt.show()
